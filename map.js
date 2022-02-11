c0f7551d02365bbd9aa3034029eb1977a58d1c51
minsikmoon <msmoon.business@gmail.com>
2022-02-12 01:00:55 +0900

    map2

--- map.js
+++ map.js
@@ -4,3 +4,16 @@ const list = [1, 2, 3];
 //process
 const multipleList = list.map(item => item * 10);
 multipleList.forEach(item => console.log(item));
+
+
+//define2
+const listEmployee = [
+    { name: 'tester1', age: 31, salary: 4500 },
+    { name: 'tester2', age: 22, salary: 3300 },
+    { name: 'tester3', age: 44, salary: 6500 },
+];
+
+//process
+const raisedSalaryList = listEmployee.map(employee => (employee.salary * 1.1));
+//console out
+raisedSalaryList.forEach(salary => console.log('salary: ',salary));
