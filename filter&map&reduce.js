d2dd26516646285995323f372a8a1bf357e28114
minsikmoon <msmoon.business@gmail.com>
2022-02-19 15:48:38 +0900

    map reduce

---
+++ filter&map&reduce.js
@@ -0,0 +1,7 @@
+const students = [
+    { name: 'tester1', age: 31, score: 85 },
+    { name: 'tester2', age: 21, score: 95 },
+    { name: 'tester3', age: 36, score: 77 },
+];
+
+//점수가 80점 이상학생 필터링 후 점수합산
