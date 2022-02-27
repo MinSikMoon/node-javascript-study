df3da687338ca34f0ebf5c2cabdc848bb183e6c6
minsikmoon <msmoon.business@gmail.com>
2022-02-28 00:36:40 +0900

    import export

--- import_export/library.js
+++ import_export/library.js
@@ -1,3 +1,8 @@
 var a = 10;
-
-export default a; //a를 기본적으로 내보내겠습니다. 
+var b = 20;
+var c = 30;
+var d = 40;
+//export default a; //a를 기본적으로 내보내겠습니다. 1번만 사용가능 // import하는쪽에서 이름 다른걸로 바꿔도 됨
+// 여러개를 export하려면 default가 아니라 중괄호로
+export { a, b, c };
+export default d; //d는 가져다 쓰는 쪽에서 작명 가능
